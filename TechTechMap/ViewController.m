682949424978925896138ebff908c8b150138d79
山﨑慎太郎 <hitorans@softbank.ne.jp>
2015-10-31 15:25:56 +0900

    version 1.0 RSSIスライダー, パーソナリティ情報など

--- TechTechMap/ViewController.m
+++ TechTechMap/ViewController.m
@@ -18,12 +18,18 @@
 {
     BOOL isScanning;
     NSArray *serviceUUIDs;
+    NSMutableDictionary *dictSubText;
+    NSMutableDictionary *dictWriteWord;
+    NSMutableDictionary *dictCharacteristic;
     NSUserDefaults *userDefault;
+    
 }
 @property (nonatomic, strong) CBCentralManager *centralManager;
 @property (nonatomic, strong) NSMutableArray *peripherals;
 @property (nonatomic, strong) IBOutlet UILabel *lblLog;
 @property (nonatomic, strong) IBOutlet UITextField *txtLocation;
+@property (nonatomic, strong) IBOutlet UILabel *lblRSSI;
+@property (nonatomic, strong) IBOutlet UISlider *sldRSSI;
 @property (nonatomic) IBOutlet UITableView *tblPeripheral;
 @end
 
@@ -50,9 +56,22 @@
     self.peripherals = [NSMutableArray array];
     serviceUUIDs = [NSArray arrayWithObjects:[CBUUID UUIDWithString:kServiceUUID], nil];
     
+    dictSubText = [NSMutableDictionary dictionary];
+    dictWriteWord = [NSMutableDictionary dictionary];
+    dictCharacteristic = [NSMutableDictionary dictionary];
+    
     // テーブル
     self.tblPeripheral.delegate = self;
     self.tblPeripheral.dataSource = self;
+    
+    // RSSIスライダー
+    if ([userDefault integerForKey:@"RSSI"] == 0) {
+        self.sldRSSI.value = 70;
+    } else {
+        self.sldRSSI.value = [userDefault integerForKey:@"RSSI"];
+    }
+    [self.sldRSSI addTarget:self action:@selector(changeSlider:) forControlEvents:UIControlEventValueChanged];
+    [self showSliderValue];
   
 }
 
@@ -101,6 +120,17 @@
         
         [self.tblPeripheral reloadData];
         
+        
+        // パーソナリティ情報取得
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+            while (isScanning) {
+                [self getParsonalityInfo];
+                sleep(30);
+            }
+        });
+
+        
+        
         // ログ
         [self showLog:@"Start Scan"];
     }
@@ -120,19 +150,25 @@
     }
 }
 
+#pragma mark - UISlider
+// 値変更
+- (void) changeSlider: (UISlider*)slider {
+    [userDefault setInteger:slider.value forKey:@"RSSI"];
+    
+    [self showSliderValue];
+}
+
+// ラベル表示
+- (void) showSliderValue {
+    self.lblRSSI.text = [NSString stringWithFormat:@"%f", self.sldRSSI.value];
+}
+
+
 #pragma mark - Write
 /// 書き込み
-- (void) write: (CBCharacteristic *) characteristic Peripheral: (CBPeripheral *) peripheral {
-    if (!(characteristic)) {
-        // ログ
-        [self showLog:@"Outoput not ready!"];
-        
-        return;
-    }
-    
-    
+- (void) write: (CBPeripheral *) peripheral {
     // 先頭に長さ情報を付加
-    NSData *txt = [self.txtLocation.text dataUsingEncoding:NSASCIIStringEncoding];
+    NSData *txt = [[dictWriteWord objectForKey:peripheral.identifier.UUIDString] dataUsingEncoding:NSASCIIStringEncoding];
     
     const unsigned char *ptr = [txt bytes];
     unsigned char value[512];
@@ -141,7 +177,7 @@
     value[2] = 0x00;
     value[3] = 0x00;
     
-    int dataLength = [txt length];
+    long dataLength = [txt length];
     for(int i = 4; i - 4 < dataLength; i++) {
         unsigned char c = *ptr++;
         value[i] = c;
@@ -154,16 +190,16 @@
     
     // 書き込み
     [peripheral writeValue:data
-              forCharacteristic:characteristic
+         forCharacteristic:[dictCharacteristic objectForKey:peripheral.identifier.UUIDString]
                            type:CBCharacteristicWriteWithResponse];
     
-    NSLog(@"%@", self.txtLocation.text);
+    NSLog(@"%@", [dictWriteWord objectForKey:peripheral.identifier.UUIDString]);
 
 }
 
 #pragma mark - POST
 // サーバーへポストする
-- (void) sendIDs: (NSString *) UUID Location: (NSString *) loc {
+- (void) postIDs: (NSString *) UUID Location: (NSString *) loc {
     // 送信したいURLを作成する
     NSURL *url = [NSURL URLWithString:@"http://52.68.204.205:3000/beacons"];
     // Mutableなインスタンスを作成し、インスタンスの内容を変更できるようにする
@@ -199,6 +235,47 @@
 //    NSLog(@"Token is %@", token);
 }
 
+// パーソナリティ情報取得
+- (void) getParsonalityInfo {
+    // 送信したいURLを作成する
+    NSURL *url = [NSURL URLWithString:@"http://52.68.204.205:3000/beacons?device=personality"];
+    // Mutableなインスタンスを作成し、インスタンスの内容を変更できるようにする
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
+    // MethodにPOSTを指定する。
+    request.HTTPMethod = @"GET";
+    
+    
+    //同期通信で送信
+    NSURLResponse *response = nil;
+    NSError *error = nil;
+    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    
+    if (error != nil) {
+        NSLog(@"Error!%@", error);
+        return;
+    }
+    
+    NSError *e = nil;
+    
+    //取得したレスポンスをJSONパース
+    NSArray *aryjson = [NSJSONSerialization JSONObjectWithData:data options:nil error:&e];
+    
+    NSLog(@"%@", aryjson);
+    
+    // 接続中のペリフェラルに書き込み
+    NSDictionary *p_dict = [aryjson objectAtIndex:0];
+    NSString *p_loc = [p_dict objectForKey:@"location"];
+    for (CBPeripheral *peripheral in self.peripherals) {
+        if (peripheral.state == CBPeripheralStateConnected) {
+            if ([dictCharacteristic objectForKey:peripheral.identifier.UUIDString] != nil) {
+                NSString *command = [@"p" stringByAppendingString:p_loc];
+                [dictWriteWord setObject:command forKey:peripheral.identifier.UUIDString];
+                [self write:peripheral];
+            }
+        }
+    }
+
+}
 
 
 #pragma mark - CBCentralManagerDelegate
@@ -211,14 +288,16 @@
 
 // ペリフェラルを発見すると呼ばれる
 - (void) centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)RSSI {
-    NSLog(@"発見したBLEデバイス：%@", peripheral);
-    NSLog(@"%d", RSSI.intValue);
     
     // NameがIMBLE0083なら接続
     if (![peripheral.name hasPrefix:@"IMBLE"]) return;
     
+    NSLog(@"Map：%@", peripheral);
+    
     if (peripheral.state == CBPeripheralStateDisconnected) {
         [self.peripherals addObject: peripheral];
+        [dictSubText setObject:@"" forKey:peripheral.identifier.UUIDString];
+        [dictWriteWord setObject:@"" forKey:peripheral.identifier.UUIDString];
     }
     
     // 接続開始
@@ -239,7 +318,7 @@
     
     peripheral.delegate = self;
     
-    [peripheral readRSSI];
+    [peripheral discoverServices:nil];
 }
 
 /// 切断成功するとよばれる
@@ -256,6 +335,8 @@
     
     // 解放
     [self.peripherals removeObject:peripheral];
+    [dictSubText removeObjectForKey:peripheral.identifier.UUIDString];
+    [dictWriteWord removeObjectForKey:peripheral.identifier.UUIDString];
     
     // テーブル更新
     [self.tblPeripheral reloadData];
@@ -310,13 +391,16 @@
         
         // Write専用のキャラクタリスティック
         if ([characteristic.UUID isEqual:[CBUUID UUIDWithString:kWriteUUID]]) {
-            // ログ
-            [self showLog:@"Start Write"];
+            // 格納
+            [dictCharacteristic setObject:characteristic forKey:peripheral.identifier.UUIDString];
             
-            [self write:characteristic Peripheral:peripheral];
+            // RSSIチェック
+            [peripheral readRSSI];
         }
 
     }
+    
+    [peripheral readRSSI];
 }
 
 // データ書き込みが完了すると呼ばれる
@@ -327,16 +411,22 @@
     }
     
     // ログ
-    [self showLog:@"Success Write"];
+    [self showLog:[NSString stringWithFormat: @"Success Write: %@", [dictWriteWord objectForKey:peripheral.identifier.UUIDString]]] ;
     
-    // 接続切断
-    [self.centralManager cancelPeripheralConnection:peripheral];
+    // 書き込み場合分け
+    // location
+    if ([[dictWriteWord objectForKey:peripheral.identifier.UUIDString] hasPrefix:@"l"]) {
+        // 接続切断
+        [self.centralManager cancelPeripheralConnection:peripheral];
+        
+        // DBへ書き込み
+        // UUID, loc
+        NSString *UUID = peripheral.identifier.UUIDString;
+        NSString *loc = self.txtLocation.text;
+        [self postIDs: UUID Location: loc];
+    }
     
-    // DBへ書き込み
-    // UUID, loc
-    NSString *UUID = peripheral.identifier.UUIDString;
-    NSString *loc = self.txtLocation.text;
-    [self sendIDs: UUID Location: loc];
+    
 
 }
 
@@ -344,8 +434,11 @@
 - (void) peripheral:(CBPeripheral *)peripheral didReadRSSI:(NSNumber *)RSSI error:(NSError *)error {
     NSLog(@"RSSI %d %@", RSSI.intValue, peripheral.identifier.UUIDString);
     
+    [dictSubText setObject:[NSString stringWithFormat:@"%d", RSSI.intValue] forKey: peripheral.identifier.UUIDString];
+    [self.tblPeripheral reloadData];
+    
     // もう一度読み込む
-    if (RSSI.intValue < -70) {
+    if (RSSI.intValue < -self.sldRSSI.value) {
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
             sleep(0.4);
             [peripheral readRSSI];
@@ -354,8 +447,13 @@
         return;
     }
     
-    // サービス探索開始
-    [peripheral discoverServices:nil];
+    // ログ
+    [self showLog:@"Start Write"];
+    
+    // 書き込み
+    [self write:peripheral];
+    // 書き込みワード指定
+    [dictWriteWord setObject:self.txtLocation.text forKey:peripheral.identifier.UUIDString];
 }
 
 #pragma mark - UITableViewDataSource
@@ -388,15 +486,20 @@
                                       reuseIdentifier:CellIdentifier];
     }
     
+    cell.textLabel.numberOfLines = 2;
+    
     CBPeripheral *peripheral = [self.peripherals objectAtIndex:indexPath.row];
     NSString *UUID = peripheral.identifier.UUIDString;
     
     // ステータスで色を変える
     switch (peripheral.state) {
         case CBPeripheralStateConnected:
+        case CBPeripheralStateConnecting:
+            
             cell.textLabel.textColor = [UIColor blueColor];
             break;
         case CBPeripheralStateDisconnected:
+        case CBPeripheralStateDisconnecting:
             cell.textLabel.textColor = [UIColor redColor];
             break;
         default:
@@ -408,7 +511,7 @@
     
     switch (indexPath.section) {
         case 0:
-            cell.textLabel.text = UUID;
+            cell.textLabel.text = [NSString stringWithFormat:@"%@\n%@", UUID, [dictSubText objectForKey:peripheral.identifier.UUIDString]];
             break;
         default:
             break;
